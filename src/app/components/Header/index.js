19a939cd3d79302b736833f5970787902ee778f2
Rishabh Anand <ranand16@gmail.com>
2023-09-25 08:06:03 +0530

    added header with social Links, icons

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,14 +1,36 @@
-import { cn } from "@/utils";
+import { GITHUB, LINKED_IN, cn } from "@/utils";
 import { FC } from "react";
 import Logo from "./Logo";
+import Link from "next/link";
+import { Github, LinkedIn } from "../Icons";
 
 const Header = () => {
     return (
-        <div
+        <header
             className={cn("w-full p-4 px-10 flex items-center justify-between")}
         >
             <Logo />
-        </div>
+            <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+                <Link className="mr-2" href="/">
+                    Home
+                </Link>
+                <Link className="mx-2" href="/about">
+                    About
+                </Link>
+                <Link className="mr-2" href="/contact">
+                    Contact
+                </Link>
+                <button>H</button>
+            </nav>
+            <div>
+                <a href={LINKED_IN}>
+                    <LinkedIn />
+                </a>
+                <a href={GITHUB}>
+                    <Github />
+                </a>
+            </div>
+        </header>
     );
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
+
+export const LINKED_IN = "https://www.linkedin.com/in/rishabh-anand-8b3b8197/";
+export const GITHUB = "https://github.com/ranand16";
