dd42678325bdaab98313c4b90b951258ec557c53
Rishabh Anand <ranand16@gmail.com>
2024-10-11 18:12:23 +0530

    removed view counter

--- src/components/Blog/BogDetails.js
+++ src/components/Blog/BogDetails.js
@@ -10,9 +10,9 @@ const BlogDetails = ({ blog, slug: slugText }) => {
     return (
         <div className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark  py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
             <time>{format(parseISO(blog.publishedAt), "LLLL d, yyyy")}</time>
-            <span>
+            {/* <span>
                 <ViewCounter slug={slugText} />
-            </span>
+            </span> */}
             <div>{blog.readingTime.text}</div>
             <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
                 #{blog.tags[0]}
